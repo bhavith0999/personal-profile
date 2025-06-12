c15fa9ca54847e2e5f4cc0947dea7acddcd24029
bhavith chandra prasad <yourgitemail@example.com>
2025-06-12 23:05:52 +0100

    Add Footer component and update layout

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-red-600">Tailwind is finally working 🎉</h1>
+    </main>
+  );
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer(){
+    return(
+        <footer className="text-centre py-4 text-sm text-gray-500">
+        © {new Date().getFullYear()} Bhavith. All rights reserved.
+        </footer>
+    )
+}
+//text-center py-4 text-sm text-gray-500
